1e95eadb4529e8c2e39218baa23af0522fc7460e
BrieucKyo <brieucmailpro@gmail.com>
2016-12-20 15:57:14 +0100

    Prototype almost finished, will be improved in the further

--- javascript/script.js
+++ javascript/script.js
@@ -17,12 +17,7 @@ function change_shoes2() {
 function change_shoes3() {
                 var image = document.getElementById('viewshoes');
                 image.style.backgroundImage = "url('images/firstblock/container/viewshoes3.png')";
-                image.style.width = "40em";
-                image.style.height = "22em";
-                image.backgroundSize = "100% 100%";
-                
-                var image = document.getElementById('viewshoes');
-                image.style.Position = "absolute";
-                image.style.top = "13em";
-                image.style.left = "8em";
+                image.style.maxwidth = "40em";
+                image.style.maxheight = "22em";
+                image.style.backgroundSize = "100% 100%";
             }
