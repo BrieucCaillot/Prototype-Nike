22850971d2450b29ff4ae85700cdefa584934527
BrieucKyo <brieucmailpro@gmail.com>
2016-12-24 02:14:54 +0100

    Added form

--- javascript/scriptjquery.js
+++ javascript/scriptjquery.js
@@ -9,7 +9,7 @@ $(window).scroll(function(){
   $(".shopright").animate({"marginBot": ($(window).scrollBottom()) + "px", }, "slow" );
 });*/
 
-	var shop = $('.shopright');
+	/*var shop = $('.shopright');
 	var pos_init = shop.offset().top;
 	shop.css("position", "absolute");
 	$(window).stop().scroll(function(){
@@ -28,11 +28,46 @@ $(window).scroll(function(){
 			});
 		}
 	});
+*/
+
+function isElementInViewport(elem) {
+    var $elem = $(elem);
+
+    // Get the scroll position of the page.
+    var scrollElem = ((navigator.userAgent.toLowerCase().indexOf('webkit') != -1) ? 'body' : 'html');
+    var viewportTop = $(scrollElem).scrollTop();
+    var viewportBottom = viewportTop + $(window).height();
+
+    // Get the position of the element on the page.
+    var elemTop = Math.round( $elem.offset().top );
+    var elemBottom = elemTop + $elem.height();
+
+    return ((elemTop < viewportBottom) && (elemBottom > viewportTop));
+}
+
+// Check if it's time to start the animation.
+function checkAnimation() {
+    var $elem = $('.bar .level');
+
+    // If the animation has already been started
+    if ($elem.hasClass('start')) return;
+
+    if (isElementInViewport($elem)) {
+        // Start the animation
+        $elem.addClass('start');
+    }
+}
 
 
+
+$(window).scroll(function(){
+    checkAnimation();
+});
 
 
 });
+
+
 
 
 
